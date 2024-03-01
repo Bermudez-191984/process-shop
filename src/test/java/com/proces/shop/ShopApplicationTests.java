39f98230d3cc62a5c89d61b13dafc65158ec685c
Jose Bermudez <jabermudezc@ufpso.edu.co>
2024-02-29 21:12:56 -0500

    init commit

---
+++ src/test/java/com/proces/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proces.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
